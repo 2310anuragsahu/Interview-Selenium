23349dcffbefbf93a5ab45fbdf6391a8c15a16eb
Anurag Sahu <Anurag.Sahu@vanderlande.com>
2020-06-18 18:52:55 +0530

    Changes for Bamboo

--- src/main/java/autoIT/AutoIT.java
+++ src/main/java/autoIT/AutoIT.java
@@ -3,7 +3,6 @@ package autoIT;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 
 public class AutoIT {
 
